6cbab42db3d6829c27d27dedab8e385f65072f8c
Shanice M <shanicemcsavaney@gmail.com>
2020-06-17 00:27:02 -0700

    Start adding mobile nav

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,68 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { NavLink } from "react-router-dom";
+import NavIcon from "./NavIcon.svg";
 
 function Header() {
+  function openMobileNav() {
+    const mobileNavMenu = document.getElementsByClassName('mobile-nav')[0];
+
+    mobileNavMenu.classList.add('open');
+  }
+
   return (
-    <header className="nav-bar center sticky-top dark-blue">
-      <p className="header-names">J + S</p>
-      <ul>
-        <li className="nav-bar-item">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li className="nav-bar-item">
-          <NavLink to="/info">Day-of Details</NavLink>
-        </li>
-        <li className="nav-bar-item">
-          <NavLink to="/rsvp">RSVP</NavLink>
-        </li>
-        <li className="nav-bar-item">
-          <NavLink to="/vancouver">Vancouver</NavLink>
-        </li>
-        <li className="nav-bar-item">
-          <NavLink to="/registry">Registry</NavLink>
-        </li>
-      </ul>
-      <hr class="dark-blue"></hr>
-    </header>
+    <div>
+      <header className="nav-bar center sticky-top dark-blue">
+        <p className="header-names">J + S</p>
+        <div className="mobile-nav-icon--bar">
+          <img 
+            src={NavIcon}
+            className="mobile-nav-icon"
+            alt="Open Mobile Navigation Button"
+            onClick={openMobileNav}
+          />
+        </div>
+        <div className="main-nav">
+          <ul>
+            <li className="nav-bar-item">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="nav-bar-item">
+              <NavLink to="/info">Day-of Details</NavLink>
+            </li>
+            <li className="nav-bar-item">
+              <NavLink to="/rsvp">RSVP</NavLink>
+            </li>
+            <li className="nav-bar-item">
+              <NavLink to="/vancouver">Vancouver</NavLink>
+            </li>
+            <li className="nav-bar-item">
+              <NavLink to="/registry">Registry</NavLink>
+            </li>
+          </ul>
+          <hr className="dark-blue"></hr>
+        </div>
+      </header>
+      <div className="mobile-nav">
+        <ul>
+          <li className="nav-bar-item">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="nav-bar-item">
+            <NavLink to="/info">Day-of Details</NavLink>
+          </li>
+          <li className="nav-bar-item">
+            <NavLink to="/rsvp">RSVP</NavLink>
+          </li>
+          <li className="nav-bar-item">
+            <NavLink to="/vancouver">Vancouver</NavLink>
+          </li>
+          <li className="nav-bar-item">
+            <NavLink to="/registry">Registry</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
