ba855eab2368494de8399d5c1453a4b37ea7ec8b
Shanice Rae <shanicemcsavaney@gmail.com>
2020-06-24 23:28:51 -0700

    Add placeholders to wedding party page

--- src/WeddingParty.js
+++ src/WeddingParty.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Maiysha from "./images/wedding-party/maiysha.jpg";
+import Faith from "./images/wedding-party/faith.jpg";
 
 function WeddingParty() {
   return (
@@ -17,47 +19,128 @@ function WeddingParty() {
       <div className="content-wrapper">
         <h3 className="content-title--nop">The Bride Tribe</h3>
         <p className="alt-content-title">Shanice's nearest and dearest</p>
-        <p>Maiysha McSavaney</p>
-        <p>Hershey Rendon</p>
-        <p>Kielle Marasigan</p>
-        <p>Faith Lopez</p>
-        <p>Brittany Sriranpong</p>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Maiysha McSavaney</p>
+            <p className="wp--description">
+              Shanice's favourite sister. She's ok.
+            </p>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--text">
+            <p className="wp--name">Hershey Rendon</p>
+            <p className="wp--description">
+              Shanice's cousin. Her son is a cute bb.
+            </p>
+          </div>
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Kielle Marasigan</p>
+            <p className="wp--description">Another cousin</p>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--text">
+            <p className="wp--name">Faith Lopez</p>
+            <p className="wp--description">Another cousin</p>
+          </div>
+          <div className="wp--img-container">
+            <img
+              src={Faith}
+              alt="Faith climbing a tree"
+              className="wp--img"
+            ></img>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Brittany Sriranpong</p>
+            <p className="wp--description">
+              Not a cousin!!!! A basic bitch who likes jello shots
+            </p>
+          </div>
+        </div>
       </div>
       <div className="content-wrapper">
         <h3 className="content-title--nop">The Groom's Crew</h3>
         <p className="alt-content-title">The ones who have Josué's back</p>
-        <p>
-          Carlos Arevalo
-          <br />
-          He’s been my older brother my WHOLE life. He loves TMNT and Tupac, but
-          hates when I tell him tupac puns (usted-pac is the formal of tupac)
-          and loves obscure 90’s references.{" "}
-        </p>
-        <p>
-          Justin Heron
-          <br />
-          Been friends with this guy since Grade 8. He’s a big history buff and
-          a bigger nerd. Need something to talk to him about? Ask about The War
-          of 1812 or Star Wars (Hope you have four hours).
-        </p>
-        <p>
-          Jason Ng
-          <br />
-          He’s my brother from another mother.{" "}
-        </p>
-        <p>
-          Ruby Bonilla
-          <br />
-          My grooms(wo)man - she earns a special place in this group. She’s my
-          sister-in-law and a good friend. She loves her family and shoes and
-          can do a wicked Jamaican accent. Just ask her!
-        </p>
-        <p>
-          Ashton Sriranpong
-          <br />
-          Shanice has known him since they were tiny and I’ve gotten to know him
-          since Shanice and I have gotten together. He’s my workout partner.
-        </p>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Carlos Arevalo</p>
+            <p className="wp--description">
+              He’s been my older brother my WHOLE life. He loves TMNT and Tupac,
+              but hates when I tell him tupac puns (usted-pac is the formal of
+              tupac) and loves obscure 90’s references.
+            </p>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--text">
+            <p className="wp--name">Justin Heron</p>
+            <p className="wp--description">
+              Been friends with this guy since Grade 8. He’s a big history buff
+              and a bigger nerd. Need something to talk to him about? Ask about
+              The War of 1812 or Star Wars (Hope you have four hours).
+            </p>
+          </div>
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Jason Ng</p>
+            <p className="wp--description">
+              He’s my brother from another mother.
+            </p>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--text">
+            <p className="wp--name">Ruby Bonilla</p>
+            <p className="wp--description">
+              My grooms(wo)man - she earns a special place in this group. She’s
+              my sister-in-law and a good friend. She loves her family and shoes
+              and can do a wicked Jamaican accent. Just ask her!
+            </p>
+          </div>
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+        </div>
+        <div className="wp--container">
+          <div className="wp--img-container">
+            <img src={Maiysha} alt="Maiysha" className="wp--img"></img>
+          </div>
+          <div className="wp--text">
+            <p className="wp--name">Ashton Sriranpong</p>
+            <p className="wp--description">
+              Shanice has known him since they were tiny and I’ve gotten to know
+              him since Shanice and I have gotten together. He’s my workout
+              partner.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
